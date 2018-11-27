3f308009d91310d9b992a9a3c76cc309fa95e6a3
Pierre Kettmus <pkettmus@gmail.com>
2018-11-27 10:47:13 +0100

    Initial Commit

---
+++ LinterV2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinterV2
+//
+//  Created by Pierre Kettmus on 27/11/2018.
+//  Copyright © 2018 Pierre Kettmus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
